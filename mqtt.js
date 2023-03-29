c82dd733ead8b718202582998e96ebaa771dafa7
David <alasdavid01@gmail.com>
2023-03-29 14:12:42 -0500

    testing more

--- mqtt.js
+++ mqtt.js
@@ -1,28 +1,28 @@
-var clientId = Math.floor(Math.random() * 10000).toString();
-client = new Paho.MQTT.Client("f88526ba972442818f936ee5c29a35e8.s2.eu.hivemq.cloud", 8884,clientId);
+// var clientId = Math.floor(Math.random() * 10000).toString();
+// client = new Paho.MQTT.Client("f88526ba972442818f936ee5c29a35e8.s2.eu.hivemq.cloud", 8884,clientId);
 
 
-//set callback handlers
-client.onConnectionLost = function (responseObject) {
-    console.log("Connection Lost: "+responseObject.errorMessage);
-}
+// //set callback handlers
+// client.onConnectionLost = function (responseObject) {
+//     console.log("Connection Lost: "+responseObject.errorMessage);
+// }
 
 
-// Called when the connection is made
-function onConnect(){
-	console.log("Connected");
-  //client.subscribe("#");
+// // Called when the connection is made
+// function onConnect(){
+// 	console.log("Connected");
+//   //client.subscribe("#");
 
-}
+// }
 
-// Connect the client, providing an onConnect callback
-client.connect({
+// // Connect the client, providing an onConnect callback
+// client.connect({
   
-	onSuccess: onConnect,
-  userName: 'karen',
-  password: "seniordesignteami13",
-  useSSL:true
-});
+// 	onSuccess: onConnect,
+//   userName: 'karen',
+//   password: "seniordesignteami13",
+//   useSSL:true
+// });
 
 document.onkeydown = checkKey; 
 document.onkeyup = checkKeyup;
